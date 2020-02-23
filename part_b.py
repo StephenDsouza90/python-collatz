4f1960accd0ac26ab62ad43f35f2470b0c074c9b
StephenDsouza90 <dsouza_stephen@hotmail.com>
2020-02-23 12:17:26 +0100

    Use loop instead of recursion for large m

--- part_b.py
+++ part_b.py
@@ -1,7 +1,7 @@
-def get_number_of_steps(m, count_steps=0):
+def get_number_of_steps(m):
     """
-    A recursive function that counts the number of steps
-    it takes for any given positive number to reach 1.
+    A function that counts the number of steps
+    it takes for any given positive number m to reach 1.
 
     The steps follow a sequence defined by 
     "Collatz conjecture" theory (See README).
@@ -11,21 +11,22 @@ def get_number_of_steps(m, count_steps=0):
     if the number is uneven then "num * 3 + 1".
 
     :param m: any given positive number from N
-    :param count_steps: the number of steps it takes to reach 1 for any given m
     :return count_steps: the number of steps it takes to reach 1 for any given m
     """
 
-    # Base condition where m = 1
-    if m == 1:
-        return int(count_steps)
-    else:
+    # Count number of steps
+    count_steps = 0
+
+    # Terminating condition where m = 1
+    while m != 1:
         # When m is even
         if m % 2 == 0:
             m = m / 2
         # When m is uneven
         else:
             m = m * 3 + 1
-        return get_number_of_steps(m, count_steps+1)
+        count_steps += 1
+    return int(count_steps)
 
 
 def get_number_of_steps_for_each_m(m_start, m_end):
