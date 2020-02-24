22b46dffd11f94ada83dcc6292eb86949cb78ce1
StephenDsouza90 <dsouza_stephen@hotmail.com>
2020-02-24 17:58:30 +0100

    Updated part d with optimization

--- part_d.py
+++ part_d.py
@@ -1,38 +1,47 @@
+import math
 import plotly.express as px
 
 
-def get_number_of_steps(m):
+def get_number_of_steps_for_each_m(m_start, m_end):
     """
-    A function that counts the number of steps
-    it takes for any given positive number m to reach 1.
+    This function optimizes the Collatz conjecture 
+    for a given range of numbers between m_start and m_end
+    and returns the number of steps to reach 1.
 
-    The steps follow a sequence defined by 
-    "Collatz conjecture" theory (See README).
-
-    Based on a starting number, 
-    if the number is even then "num / 2",
-    if the number is uneven then "num * 3 + 1".
-
-    :param m: any given positive number from N
-    :return count_steps: the number of steps it takes to reach 1 for any given m
+    :param m_start: starting number of the range
+    :param m_end: ending number of the range
+    :return all_steps: Dict with m as the key and it's corresponding steps as the value
     """
 
-    # Count number of steps
-    count_steps = 0
+    all_steps = {}
+    
+    for num in range(m_start, m_end+1):
+        m = num
+        count_steps = 0        
 
-    # Terminating condition where m = 1
-    while m != 1:
-        # When m is even
-        if m % 2 == 0:
-            m = m / 2
-        # When m is uneven
-        else:
-            m = m * 3 + 1
-        count_steps += 1
-    return int(count_steps)
+        while m != 1:
+
+            if all_steps.get(m):
+                count_steps = all_steps.get(m) + count_steps
+                break
+            
+            log_of_2 = math.log2(m)
+            if log_of_2.is_integer():
+                count_steps = log_of_2 + count_steps
+                break
+
+            if m % 2 == 0:
+                m = m / 2
+                count_steps += 1
+            else:
+                m = (m * 3 + 1) / 2
+                count_steps += 2
+
+        all_steps[num] = int(count_steps)
+    return all_steps
 
 
-def plot_graph(m_start, m_end):
+def plot_graph(listX, listY):
     """
     This function creates a graph diagram for the points (m, steps) 
     for each m in the range [m_start, m_end] and 
@@ -41,22 +50,25 @@ def plot_graph(m_start, m_end):
     The graph will be a scattered plot graph and will be represented in 
     the form of an html page that can be viewed from the browser.
 
-    :param m_start: starting value for m
-    :param m_end: end value for m
+    :param listX: list of X values (numbers m)
+    :param listY: list of Y values (number of steps for each number m)
     """
 
-    Xm = []
-    Ysteps = []
-    for m in range(m_start, m_end+1):
-        steps = get_number_of_steps(m)
-        Xm.append(m)
-        Ysteps.append(steps)
-
-    fig = px.scatter(x=Xm, y=Ysteps)
+    fig = px.scatter(x=listX, y=listY)
     fig.write_html("chart.html")
     print("\nOpen chart.html to view plotted diagram.\n")
 
 
-print("\nEnter start value followed by end value:\n")
-m_start, m_end = map(int, input().split())
-plot_graph(m_start, m_end)
+def main():
+    print("\nEnter start value followed by end value:\n")
+    m_start, m_end = map(int, input().split())
+    all_steps = get_number_of_steps_for_each_m(m_start, m_end)
+    Xm = []
+    Ysteps = []
+    for k, v in all_steps.items():
+        Xm.append(k)
+        Ysteps.append(v)
+    plot_graph(Xm, Ysteps)
+
+
+main()
